0e0001f7506f79c2ef6ec8052d845aecf5db256e
melhiour <melhiour@gmail.com>
2021-04-04 18:17:28 +0100

    Adding tests

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -122,17 +122,23 @@ def test_get_endpoint_for_operation_wrong_input():
 def test_pusher_set_commands_succesful():
     with requests_mock.Mocker() as mock:
         result = '{"success": true, "data": null, "error": null}'
+        expected = {'batched': True, 'result': {
+            "success": True, "data": None, "error": None}}
         mock.post('https://192.0.2.2/configure', text=str(result))
-        assert helpers.pusher('192.0.2.2', ['set system host-name test'], 'default', brave=True) == [
-            {"success": True, "data": None, "error": None}]
+        gotten = helpers.pusher(
+            '192.0.2.2', ['set system host-name test'], 'default', brave=True)
+        assert gotten == expected
 
 
 def test_pusher_show_commands_succesful():
     with requests_mock.Mocker() as mock:
         result = '{"success": true, "data": {"ethernet": {"eth0": {"address": "192.168.0.14/24", "hw-id": "50:01:00:04:00:00"}}}, "error": null}'
+        expected = {'batched': False, 'result': [{"success": True, "data": {"ethernet": {
+            "eth0": {"address": "192.168.0.14/24", "hw-id": "50:01:00:04:00:00"}}}, "error": None}]}
         mock.post('https://192.0.2.2/retrieve', text=str(result))
-        assert helpers.pusher('192.0.2.2', ['show inteface ethernet'], 'default', brave=True) == [{"success": True, "data": {
-            "ethernet": {"eth0": {"address": "192.168.0.14/24", "hw-id": "50:01:00:04:00:00"}}}, "error": None}]
+        gotten = helpers.pusher(
+            '192.0.2.2', ['show inteface ethernet'], 'default', brave=True)
+        assert gotten == expected
 
 
 def test_pusher_not_brave_shows_input(capsys):
@@ -141,6 +147,18 @@ def test_pusher_not_brave_shows_input(capsys):
             helpers.pusher(
                 '192.0.2.2', ['set system host-name test'], 'default')
             assert e.type == SystemExit
+
+
+def test_show_result_succesful():
+    expected = '\n# COMMAND: some_command\n# SUCCESS: True\n# ERROR: None\n# RESULT: None\n'
+    gotten = helpers.show_result(
+        'some_command', {"success": True, "data": None, "error": None})
+    assert gotten == expected
+
+
+def test_show_result_wrong_input():
+    with pytest.raises(Exception):
+        helpers.show_result(300, [])
 
 
 def test_save_config_succesful():
