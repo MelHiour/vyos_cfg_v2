39046f798faeb101010a7b8799da20f8e4b936fb
Mikhail Ermakov <melhiour@gmail.com>
2021-04-05 21:03:45 +0100

    Adding docstrings

--- helpers.py
+++ helpers.py
@@ -15,13 +15,23 @@ output = '''
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def parse_yaml(file):
-    with open(file) as file:
+def parse_yaml(filename):
+    '''
+    Converting a YAML file into Python native data structures.
+    args: filename (str)
+    returns: native data structures
+    '''
+    with open(filename) as file:
         result = yaml.load(file, Loader=yaml.FullLoader)
     return result
 
 
 def yes_or_no(question):
+    '''
+    Checks what user thinks about the question.
+    args: question (str)
+    returns: True or just exit
+    '''
     reply = str(input('\n' + question+' (y/n): ')).lower().strip()
     if reply[:1] == 'y':
         return True
@@ -33,6 +43,11 @@ def yes_or_no(question):
 
 
 def hasher(your_text, align='^'):
+    '''
+    Just to format a string using hashes.
+    args: your_text (str), align(str) = [<^]
+    returns: formatted string
+    '''
     if align == '^':
         return '{:#^100s}'.format('  '+your_text+'  ')
     elif align == '<':
@@ -42,6 +57,11 @@ def hasher(your_text, align='^'):
 
 
 def command_to_dict(command):
+    '''
+    Converts the command string into a dictionary {'op': , 'path': []}
+    args: command (str)
+    returns: dictionary
+    '''
     schema = {'op': None, 'path': None}
 
     operation = command.split()[0]
@@ -65,6 +85,12 @@ def command_to_dict(command):
 
 
 def all_config(list_of_dict):
+    '''
+    We need to check if all commands are intrusive (set,delete,comment).
+    The reason is we need to commit such commands together.
+    args: list_of_dict(list) [{'op': , 'path': []}, {'op': , 'path': []}]
+    returns: True/False
+    '''
     intrusive_commands = {'set', 'delete', 'comment'}
     set_of_operations = set(command['op'] for command in list_of_dict)
     if set_of_operations <= intrusive_commands:
@@ -76,6 +102,10 @@ def all_config(list_of_dict):
 def prepare_data(data, api_key):
     '''
     Creating a dictionary which can be accepted by VyOS
+    args: 
+    data(native data structures), 
+    api_key(str) - the name of the key from key.py
+    returns: to_push(dict)
     {'data':(None,DATA),{'key':(None,KEY)}
     '''
     to_push = {}
@@ -85,6 +115,11 @@ def prepare_data(data, api_key):
 
 
 def get_endpoint_for_operation(operation):
+    '''
+    Returning the name of endpoint based on operation
+    args: operation (str)
+    returns: endpoint (str)
+    '''
     if 'showConfig' in operation:
         return 'retrieve'
     elif 'show' in operation:
@@ -96,6 +131,12 @@ def get_endpoint_for_operation(operation):
 
 
 def save_needed(command_list):
+    '''
+    We need to know if we need to save after pushing the command list.
+    Execution of show-like commands only does not require it.
+    args: command_list(list)
+    returns: True/False
+    '''
     intrusive_commands = {'set', 'delete', 'comment'}
     set_of_operations = set(command.split()[0] for command in command_list)
     if not intrusive_commands.isdisjoint(set_of_operations):
@@ -105,6 +146,18 @@ def save_needed(command_list):
 
 
 def pusher(target, port, command_list, api_key, brave=False):
+    '''
+    Main function to push the data using API  
+    args: 
+    target(str) - IP address or hostname
+    port(str) - TCP port
+    command_list (list) - the list of commands to push
+    api_key(str) - the name of the key from key.py
+    brave(True/False) - skipping Yes/No question before intrusive commands
+    returns: dictionary with keys
+       'batched': True/False (shows if all commands have been commited together
+       'result': list of dictionaries per command or just one dictionary in case of batch
+    '''
     list_of_dict = [command_to_dict(command) for command in command_list]
 
     if all_config(list_of_dict):
@@ -130,6 +183,12 @@ def pusher(target, port, command_list, api_key, brave=False):
 
 
 def show_result(command, result):
+    '''
+    Helps formatting the output of the command sent.
+    Formatting strings differently. Needed mainly for get commands.
+    args: command (str)  
+    returns: result (dict)
+    '''
     if isinstance(result['data'], str):
         outcome = result['data'].splitlines()
     else:
@@ -141,6 +200,13 @@ def show_result(command, result):
 
 
 def save_config(target, port, api_key):
+    '''
+    Just saving the configuration.
+    args: 
+    target(str) - IP address or hostname
+    port(str) - TCP port
+    api_key(str) - the name of the key from key.py
+    '''
     url = 'https://{}:{}/config-file'.format(target, port)
     data = {"op": "save"}
     to_push = prepare_data(data, api_key)
